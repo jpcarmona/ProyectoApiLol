d751f9cb236fe5c2f145e0580dbda222f8e13162
jpcarmona <jpcarmona92@gmail.com>
2018-05-30 20:42:31 +0200

    Ediciones varias empezando

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, url_for,render_template,request,abort
 import requests
 
+app = Flask(__name__)
+
+port = 5000
+
+@app.route('/')
+def inicio():
+	return render_template('index.html')
+
 def get_requests(key,url):
 	cabecera={"Accept": "application/json", "user-key": key}
 	r1=requests.get(url,headers=cabecera)
@@ -9,10 +17,4 @@ def get_requests(key,url):
 		return doc
 
 
-@app.route("/")
-def inicio():
-	doc = etree.parse('museos.xml')
-	museos=doc.xpath('//SimpleData[@name="NOMBRE"]/text()')
-	ids=doc.xpath('//SimpleData[@name="FID"]/text()')
-	datos=zip(ids,museos)
-	return render_template("inicio.html",datos=datos)
+app.run('0.0.0.0',int(port), debug=True)
