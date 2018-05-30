c4464bd33a7ef82077c683d6f52b2da90141452f
jpcarmona <jpcarmona92@gmail.com>
2018-05-30 21:19:29 +0200

    iniciando heroku

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-from flask import Flask, url_for,render_template,request,abort
+from flask import Flask, url_for, render_template, request, session
 import requests
+import os
 
 app = Flask(__name__)
-
-port = 5000
+port = os.environ['PORT']
 
 @app.route('/')
 def inicio():
 	return render_template('index.html')
 
-def get_requests(key,url):
-	cabecera={"Accept": "application/json", "user-key": key}
-	r1=requests.get(url,headers=cabecera)
-	if r1.status_code == 200:
-		doc = r1.json()
-		return doc
+
 
 
 app.run('0.0.0.0',int(port), debug=True)
