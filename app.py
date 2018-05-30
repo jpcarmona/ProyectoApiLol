0d4271e223fdf5b8eee743d9a03036396b57fb9d
jpcarmona <jpcarmona92@gmail.com>
2018-05-30 21:25:40 +0200

    iniciando heroku3

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, url_for, render_template, request, session
-import requests
-import os
+#from flask import Flask, url_for, render_template, request, session
+#import requests
+#import os
 
-app = Flask(__name__)
-port = os.environ['PORT']
+#app = Flask(__name__)
+#port = os.environ['PORT']
 
-@app.route('/')
-def inicio():
-	return render_template('index.html')
-
-
+#@app.route('/')
+#def inicio():
+#	return render_template('index.html')
 
 
-app.run('0.0.0.0',int(port), debug=True)
+
+
+#app.run('0.0.0.0',int(port), debug=True)
