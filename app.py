72262e546f53d57aea727c4223f2cf564fa87731
jpcarmona <jpcarmona92@gmail.com>
2018-06-05 11:55:45 +0200

    cambiando plantilla

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
 from flask import Flask, url_for, render_template, request, session
-import requests
+from funciones import *
 import os
 
 app = Flask(__name__)
 port = os.environ['PORT']
 
-@app.route('/')
+apikey='RGAPI-037dca0f-a2a3-4901-86fe-d0a5941963d6'
+region='euw1'
+
+@app.route('/',methods=["post","get"])
 def inicio():
-	return render_template('index.html')
+	if request.method == "GET":
+		return render_template('index.html')
+	else:
+		nombre = request.form.get("nombre")
+		doc_info=get_info(apikey,nombre,region)
+		return render_template('perfil.html',doc=doc_info)
+
+#@app.route('/buscaperfil')
+#def buscaperfil():
+#	nombre = request.form.get("nombre")
+#	payload={"api_key":apikey}
+#	r=requests.get('https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+nombre,params=payload)
+#	if r.status_code==200:
+#		res=r.json()
+#		nombreusuario=res['name']
+#		profile=res['profileIconId']
+#		level=res['summonerLevel']
+#		idcuenta=res['accountId']
+#		id1=res['id']
+#		return render_template('perfil.html',nombre=nombreusuario,imagen=profile,nivel=level,idcuenta=idcuenta,id1=id1)
+
+#@app.route('/perfil',methods=["post","get"])
+#def perfil():
+#	nombre = request.form.get("nombre")
+#	payload={"api_key":apikey}
+#	r=requests.get('https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+nombre,params=payload)
+#	if r.status_code==200:
+#		res=r.json()
+#		nombreusuario=res['name']
+#		profile=res['profileIconId']
+#		level=res['summonerLevel']
+#		idcuenta=res['accountId']
+#		id1=res['id']
+#		return render_template('perfil.html',nombre=nombreusuario,imagen=profile,nivel=level,idcuenta=idcuenta,id1=id1)
+
+
+
 
 
 
---
+++ funciones.py
@@ -0,0 +1,30 @@
+import requests
+
+## realización de requests general
+def get_requests(apikey,url):
+	cabecera={"api_key":apikey}
+	r1=requests.get(url,params=cabecera)
+	if r1.status_code == 200:
+		return r1.json()
+	else:
+		return {}
+
+## obtener version actual del juego
+def get_version(apikey,region):
+	url='https://'+region+'.api.riotgames.com/lol/static-data/v3/versions'
+	doc_req=get_requests(apikey,url)
+	return doc_req[0]
+
+
+## informacion jugador
+def get_info(apikey,nombre,region):
+	url='https://'+region+'.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+nombre
+	doc_req=get_requests(apikey,url)
+	if doc_req:
+		version=get_version(apikey,region)
+		doc_req['icono']='http://ddragon.leagueoflegends.com/cdn/'+version+'/img/profileicon/'+str(doc_req['profileIconId'])+'.png'
+		return doc_req
+	else:
+		return {}
+
+
