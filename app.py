c1b59c4f56c42dcaa702f077ffbbb4300c3ab06a
jpcarmona <jpcarmona92@gmail.com>
2018-06-06 15:13:16 +0200

    cambios de archivos y modificaciones en el codigo1

--- app.py
+++ app.py
@@ -5,19 +5,31 @@ import os
 app = Flask(__name__)
 port = os.environ['PORT']
 
-apikey='RGAPI-0b9dffce-42bf-4d53-b6ca-94fbfdaff1f9'
+apikey='RGAPI-12d9f866-273b-4c44-b14e-987ed3700b53'
 
 @app.route('/',methods=["post","get"])
 def inicio():
 	if request.method == "GET":
-		return render_template('index.html')
+		plantilla =('index.html')
+		lista=[]
 	else:
-		nombre = request.form.get("nombre")
-		region = request.form.get("region")
-		doc_info = get_info(apikey,nombre,region)
-		doc_actual = get_actual(apikey,doc_info['id'],region)
-		jugadores = doc_actual['participants']
-		return render_template('perfil.html',doc_info=doc_info,doc_actual=doc_actual,jugadores=jugadores)
+		nombre =request.form.get("nombre")
+		region =request.form.get("region")
+		doc_info =get_info(apikey,nombre,region)
+		if doc_info:
+			doc_actual =get_actual(apikey,doc_info['id'],region)
+			jugadores =doc_actual['participants']
+			lista=[doc_info,doc_actual,jugadores]
+			plantilla =('perfil.html')
+		else:
+			plantilla =('index.html')
+			lista=[1]
+	return render_template(plantilla,lista=lista)
+
+@app.route('/perfil',methods=["post","get"])
+def perfil():
+	if request.method == "GET":
+		return render_template('perfil.html')
 
 @app.route('/historial',methods=["post","get"])
 def historial():
--- funciones.py
+++ funciones.py
@@ -1,5 +1,6 @@
 import requests
 
+version='8.11.1'
 ## realización de requests general
 def get_requests(apikey,url):
 	cabecera={"api_key":apikey}
@@ -21,7 +22,7 @@ def get_info(apikey,nombre,region):
 	url='https://'+region+'.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+nombre
 	doc_req=get_requests(apikey,url)
 	if doc_req:
-		version=get_version(apikey,region)
+		#version=get_version(apikey,region)
 		doc_req['icono']='http://ddragon.leagueoflegends.com/cdn/'+version+'/img/profileicon/'+str(doc_req['profileIconId'])+'.png'
 		return doc_req
 	else:
@@ -31,9 +32,19 @@ def get_info(apikey,nombre,region):
 def get_actual(apikey,idinvocador,region):
 	url='https://'+region+'.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/'+str(idinvocador)
 	doc_req=get_requests(apikey,url)
-	if doc_req:
-		return doc_req
-	else:
-		return {}
+	return doc_req
 
 
+## posicion rankeds
+def get_liga(apikey,idinvocador,region):
+	url='https://'+region+'.api.riotgames.com/lol/eague/v3/positions/by-summoner/'+str(idinvocador)
+	doc_req=get_requests(apikey,url)
+	return doc_req
+
+#	tipo=res['queueType']
+#	nombreliga=res['leagueName']
+#	nivel1=res['tier']
+#	nivel2=res['rank']
+#	puntos=res['leaguePoints']
+#	partidasganadas=res['wins']
+#	partidasperdidas=res['losses']
