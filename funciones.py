b3115a512a22c3fcef63e439521b3999cfc0a1ce
jpcarmona <35558113+jpcarmona@users.noreply.github.com>
2018-06-14 14:47:46 +0200

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -37,7 +37,7 @@ def get_fecha(milisegundos):
 def get_tiempo(milisegundos):
 	segundos=(milisegundos/1000) -180 ## retraso de partida
 	tiempo=datetime.datetime.now()-datetime.datetime.fromtimestamp(segundos)
-	minutos=int(tiempo.seconds/60)
+	int(tiempo.seconds)/60
 	return minutos
 
 ## informacion jugador
